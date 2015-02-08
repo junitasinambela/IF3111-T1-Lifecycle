688189eea16221e34266decda2b407ce31e1c5c5
Junita Sinambela <jujunale@gmail.com>
2015-02-08 12:50:25 +0700

    L

--- com.junita.android.intent.explicit/app/src/main/java/com/example/user/comjunitaandroidintentexplicit/MainActivity.java
+++ com.junita.android.intent.explicit/app/src/main/java/com/example/user/comjunitaandroidintentexplicit/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.user.comjunitaandroidintentexplicit;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-
 public class MainActivity extends Activity {
+
+    // constant to determine which sub-activity returns
+    private static final int REQUEST_CODE = 10;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,13 +20,23 @@ public class MainActivity extends Activity {
 
     public void onClick(View view) {
         EditText text = (EditText) findViewById(R.id.inputforintent);
-        // used later
-        String value = text.getText().toString();
-        // TODO 1 create new Intent(context, class)
-        // use the activity as context parameter
-        // and "ResultActivity.class" for the class parameter
-
-        // TODO 2 start second activity with
-        // startActivity(intent);
+        String string = text.getText().toString();
+        Intent i = new Intent(this, ResultActivity.class);
+        i.putExtra("yourkey", string);
+        // TODO 2.. now use
+        // startActivityForResult(i, REQUEST_CODE);
     }
-}
+
+    // TODO 3 Implement this method
+    // assumes that "returnkey" is used as key to return the result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data.hasExtra("returnkey")) {
+                String result = data.getExtras().getString("returnkey");
+                if (result != null && result.length() > 0) {
+                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }

2f1dd2538b13b13071665f7f3728a230fdfc632f
Junita Sinambela <jujunale@gmail.com>
2015-02-08 12:31:51 +0700

    Project I, II, dan III

---
+++ com.junita.android.intent.explicit/app/src/main/java/com/example/user/comjunitaandroidintentexplicit/ResultActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.comjunitaandroidintentexplicit;
+
+/**
+ * Created by user on 2/7/2015.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity{
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_result);
+    }
+}
